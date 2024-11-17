f8de5799b19e4a1dab05ed3e20ad9cd7ff13f2b3
Alina Boess <156015430+AlinaBoess@users.noreply.github.com>
2024-11-17 16:23:51 +0100

    Adding Frontend for login

---
+++ programCode/wwwroot/login.js
@@ -0,0 +1,31 @@
+﻿document.getElementById("loginForm").addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const loginData = {
+        email: email,
+        password: password
+    };
+
+    try {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem('token', data.token);
+            window.location.href = '/dashboard';
+        }
+        else {
+            console.error("Anmeldung fehlgeschlagen", response.statusText);
+        }
+    } catch (error) {
+        console.error("Fehler bei der Anmeldung", error)
+    }
+});
+
