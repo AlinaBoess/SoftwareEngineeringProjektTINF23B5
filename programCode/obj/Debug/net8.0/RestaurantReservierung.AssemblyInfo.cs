44f93fb38fefbb28cfd7b4177c2778e23e4cd9f0
Alex_3709537 <alex.fleig@online.de>
2024-12-09 16:51:31 +0100

    Fix

--- programCode/obj/Debug/net8.0/RestaurantReservierung.AssemblyInfo.cs
+++ programCode/obj/Debug/net8.0/RestaurantReservierung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantReservierung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce852d6ccad2a637d888f5b91f01c557fae23e68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+454afd69721da4ccd85a632d7be1fe4ea07cc3d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantReservierung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantReservierung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
