bb4b8de1570031e8b74d70ce11f936435b38762e
Alex_3709537 <alex.fleig@online.de>
2025-05-07 11:17:38 +0200

    refactor: remove unused code

--- programCode/Services/AdminService.cs
+++ programCode/Services/AdminService.cs
@@ -4,21 +4,6 @@ namespace RestaurantReservierung.Services
 {
     public static class AdminService
     {
-        public static bool CreateRestaurant(ref ReservationSystem reservationSystem, Restaurant r)
-        {
-            if (reservationSystem == null || reservationSystem.Restaurants == null || r == null || reservationSystem.Restaurants.Contains(r))
-                return false;
 
-            reservationSystem.Restaurants.Add(r);
-            return true;
-        }
-
-        public static bool DeleteRestaurant(ref ReservationSystem reservationSystem, Restaurant r)
-        {
-            if (reservationSystem == null || reservationSystem.Restaurants == null || r == null)
-                return false;
-
-            return reservationSystem.Restaurants.Remove(r);
-        }
     }
 }
