6141d08a532e474f4c9940f611dcb35f3f812b5e
Alina Boess <156015430+AlinaBoess@users.noreply.github.com>
2025-05-18 22:07:32 +0200

    Restaurants werden jetzt aus der Datenbank geladen.

--- programCode/obj/Debug/net8.0/RestaurantReservierung.AssemblyInfo.cs
+++ programCode/obj/Debug/net8.0/RestaurantReservierung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantReservierung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f317d27d317fd44970399f0de77dff38b13dfa6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12764c779fae6e08318c99f3f63eb3ddf1ba2899")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantReservierung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantReservierung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
