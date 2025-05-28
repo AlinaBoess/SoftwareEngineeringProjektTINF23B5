a391d285ddc913bd211cc91ea1590248fad08cf8
Alex_3709537 <alex.fleig@online.de>
2025-05-28 14:42:28 +0200

    feat: Add middleware
    - A middleware was added which checks if the user in the jwt token actually exists

---
+++ programCode/webApi/Middlewares/UserExistsMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using RestaurantReservierung.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RestaurantReservierung.Middlewares
+{
+    public class UserExistsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<UserExistsMiddleware> _logger;
+
+        public UserExistsMiddleware(RequestDelegate next, ILogger<UserExistsMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var authorizationHeader = context.Request.Headers["Authorization"].FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(authorizationHeader) && authorizationHeader.StartsWith("Bearer "))
+            {
+                var token = authorizationHeader.Substring("Bearer ".Length).Trim();
+
+                var handler = new JwtSecurityTokenHandler();
+                try
+                {
+                    var jwtToken = handler.ReadJwtToken(token);
+                    var emailClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email || c.Type == "email");
+
+                    if (emailClaim != null)
+                    {
+                
+                        var userService = context.RequestServices.GetRequiredService<UserService>();
+                        var user = await userService.GetUserByEmailAsync(emailClaim.Value);
+                        if (user == null)
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                            context.Response.ContentType = "application/json";
+
+                            await context.Response.WriteAsJsonAsync(new { Message = "The user in the JWT token does not exist." });
+                            return;
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    _logger.LogError(ex, "Error parsing JWT token.");
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    context.Response.ContentType = "application/json";
+
+                    await context.Response.WriteAsJsonAsync(new { Message = "Invalid JWT token." });
+                    return;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
